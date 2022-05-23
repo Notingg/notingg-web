27938bf36cce8792270eeda4bf44bdc960ecc580
DehLuizSantos <and_consul@outlook.com>
2022-05-22 21:08:55 -0300

    feature(search-input):componentes-commun

--- src/components/communs/SearchInput/index.tsx
+++ src/components/communs/SearchInput/index.tsx
@@ -2,20 +2,24 @@ import React, { InputHTMLAttributes } from 'react'
 import * as S from './styles'
 import { useField } from '@unform/core';
 
-type InputProps = {
+
+type SearchInputProps = {
   label?: string;
   placeholder?: string;
   name: string;
+  onSubmit?(data: any): void;
+  iconPosition: "right" | "left";
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const Input: React.FC<InputProps> = ({ placeholder, label, name }) => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+export const SearchInput: React.FC<SearchInputProps> = (
+  { placeholder, label, iconPosition, name, onSubmit }) => {
+  const SearchInputRef = React.useRef<HTMLInputElement>(null);
   const { fieldName, defaultValue, registerField, error, clearError } = useField(name);
 
   React.useEffect(() => {
     registerField({
       name: fieldName,
-      ref: inputRef,
+      ref: SearchInputRef,
       getValue: (ref) => {
         return ref.current.value;
       },
@@ -27,17 +31,30 @@ export const Input: React.FC<InputProps> = ({ placeholder, label, name }) => {
       }
     });
   }, [fieldName, registerField]);
+
+  const onClickSubmit = React.useCallback(
+    (data: any) => {
+      if (onSubmit) {
+        onSubmit(data);
+      }
+    },
+    [onSubmit]
+  );
   return (
-    <>
+    <S.SearchInputWrapper>
       {label && <S.Label htmlFor={fieldName}>{label}</S.Label>}
-      <S.InputComponent
+      <S.SearchInputComponent
         placeholder={placeholder}
         onFocus={clearError}
         autoComplete={'on'}
         defaultValue={defaultValue}
         name={fieldName}
+        type='search'
       />
       {error && <span className="validate-error">{error}</span>}
-    </>
+      <S.Icon iconPosition={iconPosition} onClick={onClickSubmit} type="button">
+        <img src='/icons/searchIcon.svg' />
+      </S.Icon>
+    </S.SearchInputWrapper>
   )
 }
